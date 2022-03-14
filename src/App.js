30868f44b96010d9bb2feae18a815a154a065009
ahmki <aleksi.heinimaki1@gmail.com>
2022-03-14 13:10:51 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import { ReactComponent as DesktopDivider } from './images/pattern-divider-desktop.svg'
 
 function App() {
+  const [advice, setAdvice] = useState('')
+  const [adviceIndex, setAdviceIndex] = useState(0)
+
+  useEffect(() => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then(res => {
+        setAdvice(res.data.slip.advice)
+        setAdviceIndex(res.data.slip.id)
+      })
+  })
+
   return (
-    <div>
-      <h1 className="text-purple-500">helo</h1>
+    <div className="flex min-h-screen justify-center items-center">
+      <div className="container rounded-xl mx-auto justify-center bg-darkgrayishblue w-96 h-60">
+        <p className="font-manrope text-neongreen text-center pt-7">Advice #{adviceIndex}</p>
+        <p className="text-2xl text-lightcyan px-7 py-7 text-center">"{advice}"</p>
+        <DesktopDivider />
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    },
+    colors: {
+      grayishblue: '#4e5d73',
+      darkgrayishblue: '#323a49',
+      darkblue: '#1f2632',
+      lightcyan: '#cee3e9',
+      neongreen: '#52ffa8'
+    },
   },
   plugins: [],
 }
